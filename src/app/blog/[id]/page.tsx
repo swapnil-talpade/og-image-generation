cb85d3d8d58db1b79eafd90121306d054ff3676f
swapnil-talpade <swapniltalpade41@gmail.com>
2024-02-13 19:55:57 +0530

    add dynamic meta data

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,5 +1,25 @@
 import Head from "next/head";
 
+export async function generateMetadata({ params }: any) {
+  const ogImage = `https://og-image-generation-olive.vercel.app/og?title=${params.id}`;
+
+  return {
+    title: params.id,
+    openGraph: {
+      title: params.id,
+      url: "./",
+      images: [
+        {
+          url: ogImage,
+          width: 1200,
+          height: 630,
+          alt: params.id,
+        },
+      ],
+    },
+  };
+}
+
 const BlogDetails = ({ params }: any) => {
   const imgUrl = `https://og-image-generation-olive.vercel.app/_static/blog-og-card.png?title=${params.title}`;
 
