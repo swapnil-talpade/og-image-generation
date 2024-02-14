c5132478a862273714804bbfa797d32ddc6f5af5
swapnil-talpade <swapniltalpade41@gmail.com>
2024-02-14 18:33:02 +0530

    chore: remove try catch

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -5,41 +5,37 @@ import { ImageResponse } from "next/og";
 export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
-  try {
-    const { searchParams } = request.nextUrl;
+  const { searchParams } = request.nextUrl;
 
-    const hasTitle = searchParams.has("title");
+  const hasTitle = searchParams.has("title");
 
-    let title = hasTitle ? searchParams.get("title") : "Open Graph";
+  let title = hasTitle ? searchParams.get("title") : "Open Graph";
 
-    return new ImageResponse(
-      (
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          position: "relative",
+          color: "white",
+          display: "flex",
+          backgroundImage: `url(https://og-image-generation-olive.vercel.app/_static/blog-og-card.png)`,
+        }}
+      >
         <div
           style={{
-            height: "100%",
-            width: "100%",
-            position: "relative",
-            color: "white",
-            display: "flex",
-            backgroundImage: `url(https://og-image-generation-olive.vercel.app/_static/blog-og-card.png)`,
+            width: 744,
+            fontSize: 40,
+            position: "absolute",
+            left: 240,
+            top: 150,
+            color: "black",
           }}
         >
-          <div
-            style={{
-              width: 744,
-              fontSize: 40,
-              position: "absolute",
-              left: 240,
-              top: 150,
-              color: "black",
-            }}
-          >
-            {title}
-          </div>
+          {title}
         </div>
-      )
-    );
-  } catch (error) {
-    console.error(error);
-  }
+      </div>
+    )
+  );
 }
